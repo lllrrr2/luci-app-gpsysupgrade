28db212437f72e6807da381831c1021e144a70ad
garypang13 <48883331+garypang13@users.noreply.github.com>
2020-10-06 10:01:38 +0800

    Update system_update.lua

--- luasrc/model/cbi/gpsysupgrade/client/system_update.lua
+++ luasrc/model/cbi/gpsysupgrade/client/system_update.lua
@@ -4,10 +4,7 @@ local appname = "gpsysupgrade"
 m = Map(appname)
 
 -- [[ App Settings ]]--
-s = m:section(TypedSection, "gpsysupgrade", translate("System Upgrade"),
-              "<font color='red'>" ..
-                  translate("Please confirm that your firmware supports FPU.") ..
-                  "</font>")
+s = m:section(TypedSection, "gpsysupgrade", translate("System Upgrade"))
 s.anonymous = true
 
 s:append(Template("gpsysupgrade/system_update/system_version"))
